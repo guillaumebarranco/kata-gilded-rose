fe9d6a4cf23a1db72b01ed4875bf8c10244d4637
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2017-09-21 16:31:35 +0200

    Update tests in TDD and functionnalities

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,23 +1,86 @@
 export class Item {
-  name: string;
-  sellIn: number;
-  quality: number;
+    name: string;
+    sellIn: number;
+    quality: number;
+    getBetter: boolean;
 
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
+    constructor(name, sellIn, quality, getBetter = false) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+        this.getBetter = getBetter;
+    }
+
+    updateQuality() {
+
+        if(this.quality < 50) {
+            this.quality++;
+        }
+    }
+
+    decreaseQuality() {
+
+        if(this.quality > 0) {
+            this.quality--;
+        }
+    }
 }
 
 export class Shop {
-  items: Item[];
+    items: Item[];
 
-  constructor(items = []) {
-    this.items = items;
-  }
+    constructor(items = []) {
+        this.items = items;
+    }
 
-  updateQuality(): Item[] {
-    return this.items;
-  }
+    updateQuality(): Item[] {
+
+        this.items.forEach((element, i) => {
+
+            if(element.name === "Sulfuras") {
+                element.quality = 80;
+                return;
+            }
+
+            if(element.sellIn > 0) {
+                element.sellIn--;   
+            }
+
+            const fuckingItem = [
+                "Backstage"
+            ];
+
+            if(element.getBetter) {
+
+                element.updateQuality();
+
+                // handle fucking item
+
+                if(fuckingItem.indexOf(element.name) !== -1 && element.sellIn === 0) {
+
+                    element.quality = 0;
+
+                } else if(fuckingItem.indexOf(element.name) !== -1 && element.sellIn <= 5) {
+
+                    element.updateQuality();
+                    element.updateQuality();
+
+                } else if(fuckingItem.indexOf(element.name) !== -1 && element.sellIn <= 10) {
+                    element.updateQuality();
+                }
+
+            } else {
+
+                if(element.quality > 0 && element.sellIn > 0) {
+                    element.decreaseQuality();
+
+                } else if(element.quality > 0 && element.sellIn === 0) {
+                    element.decreaseQuality();
+                    element.decreaseQuality();
+                }
+            }
+        });
+
+        return this.items;
+    }
 }
--- test/guilded-rose.spec.ts
+++ test/guilded-rose.spec.ts
@@ -1,10 +1,211 @@
 import { expect } from "chai";
 import { Item, Shop } from "../app/gilded-rose";
 
-describe("Gilded Rose", function() {
-  let items: Item[];
+describe("Gilded Rose", () => {
 
-  beforeEach(function() {
-    items = [];
-  });
+    let items: Item[];
+
+    beforeEach(() => {
+        items = [];
+    });
+
+    it("decreases by 1 quality and remaining sellIn days of regular item", () => {
+
+        // Given
+        items.push(new Item("Dexterity Vest", 10, 20));
+        items.push(new Item("Conjured Mana Cake", 3, 6));
+
+        // When
+        items = new Shop(items).updateQuality();
+
+        // Then
+        const expected = [{
+            sellIn: 9,
+            quality: 19
+        }, {
+            sellIn: 2,
+            quality: 5
+        }];
+
+        expected.forEach((testCase, i) => {
+            expect(items[i].quality).to.equal(testCase.quality);
+            expect(items[i].sellIn).to.equal(testCase.sellIn);
+        });
+    });
+
+    it('increases the quality by 1 of the products that get better as they age', () => {
+
+        // Given
+        items.push(new Item("Aged Brie", 20, 30, true));
+        items.push(new Item('Backstage', 20, 30, true));
+
+        // When
+        items = new Shop(items).updateQuality();
+
+        // Then
+        const expected = [{
+            sellIn: 19,
+            quality: 31
+        }, {
+            sellIn: 19,
+            quality: 31
+        }];
+
+        expected.forEach((testCase, i) => {
+            expect(items[i].quality).to.equal(testCase.quality);
+            expect(items[i].sellIn).to.equal(testCase.sellIn);
+        });
+    });
+
+    it('increases the quality by 2 of the products that get better as they age when there is less than 10 days', () => {
+
+        // Given
+        items.push(new Item('Backstage', 8, 30, true));
+
+        // When
+        items = new Shop(items).updateQuality();
+
+        // Then
+        const expected = [{
+            sellIn: 7,
+            quality: 32
+        }];
+
+        expected.forEach((testCase, i) => {
+            expect(items[i].quality).to.equal(testCase.quality);
+            expect(items[i].sellIn).to.equal(testCase.sellIn);
+        });
+    });
+
+    it('increases the quality by 3 of the products that get better as they age when there is less than 5 days', () => {
+
+        // Given
+        items.push(new Item('Backstage', 4, 30, true));
+
+        // When
+        items = new Shop(items).updateQuality();
+
+        // Then
+        const expected = [{
+            sellIn: 3,
+            quality: 33
+        }];
+
+        expected.forEach((testCase, i) => {
+            expect(items[i].quality).to.equal(testCase.quality);
+            expect(items[i].sellIn).to.equal(testCase.sellIn);
+        });
+    });
+
+    it('backstage should have 0 quality as there is no day left', () => {
+
+        // Given
+        items.push(new Item('Backstage', 0, 30, true));
+
+        // When
+        items = new Shop(items).updateQuality();
+
+        // Then
+        const expected = [{
+            sellIn: 0,
+            quality: 0
+        }];
+
+        expected.forEach((testCase, i) => {
+            expect(items[i].quality).to.equal(testCase.quality);
+            expect(items[i].sellIn).to.equal(testCase.sellIn);
+        });
+    });
+
+    it('decreases quality but never under 0', () => {
+
+        // Given
+        items.push(new Item("Dexterity Vest", 10, 0));
+        items.push(new Item("Conjured Mana Cake", 3, 0));
+
+        // When
+        items = new Shop(items).updateQuality();
+
+        // Then
+        const expected = [{
+            sellIn: 9,
+            quality: 0
+        }, {
+            sellIn: 2,
+            quality: 0
+        }];
+
+        expected.forEach((testCase, i) => {
+            expect(items[i].quality).to.equal(testCase.quality);
+            expect(items[i].sellIn).to.equal(testCase.sellIn);
+        });
+    });
+
+    it('increases quality bu never more than 50', () => {
+
+        // Given
+        items.push(new Item("Aged Brie", 20, 50, true));
+        items.push(new Item('Backstage', 20, 50, true));
+
+        // When
+        items = new Shop(items).updateQuality();
+
+        // Then
+        const expected = [{
+            sellIn: 19,
+            quality: 50
+        }, {
+            sellIn: 19,
+            quality: 50
+        }];
+
+        expected.forEach((testCase, i) => {
+            expect(items[i].quality).to.equal(testCase.quality);
+            expect(items[i].sellIn).to.equal(testCase.sellIn);
+        });
+    });
+
+    it('Sulfuras product should not lose any days to sale nor quality', () => {
+
+        // Given
+        items.push(new Item("Sulfuras", 10, 50, true));
+
+        // When
+        items = new Shop(items).updateQuality();
+
+        // Then
+        const expected = [{
+            sellIn: 10,
+            quality: 80
+        }];
+
+        expected.forEach((testCase, i) => {
+            expect(items[i].quality).to.equal(testCase.quality);
+            expect(items[i].sellIn).to.equal(testCase.sellIn);
+        });
+    });
+
+    it("decreases by 2 quality of items which have not days left", () => {
+
+        // Given
+        items.push(new Item("Dexterity Vest", 0, 20));
+        items.push(new Item("Conjured Mana Cake", 0, 6));
+
+        // When
+        items = new Shop(items).updateQuality();
+
+        // Then
+        const expected = [{
+            sellIn: 0,
+            quality: 18
+        }, {
+            sellIn: 0,
+            quality: 4
+        }];
+
+        expected.forEach((testCase, i) => {
+            expect(items[i].quality).to.equal(testCase.quality);
+            expect(items[i].sellIn).to.equal(testCase.sellIn);
+        });
+    });
 });
